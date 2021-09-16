88ea33693e02869ce3d40f422130beb5e2f96d2b
Zihao Xu <zihao.xu@columbia.edu>
2021-09-16 18:21:20 -0400

    pass cache to gpu

--- notebooks/CudaKernels.cpp
+++ notebooks/CudaKernels.cpp
@@ -1,9 +1,43 @@
 #include <stdio.h>
 
-__device__ float* copy_farray(float *arr, int length)
+///////////
+// utils //
+///////////
+__device__ float* copy_farray(float *arr, int length, float *cache);
+__device__ int*   copy_iarray(int *arr, int length, int *cache);
+__device__ void   set_farray_values(float *arr, float *values, int length);
+__device__ void   set_iarray_values(int *arr, int *values, int length);
+__device__ void   apply_map_to_farray(float *arr, int *map, int length, float *cache);
+__device__ void   apply_map_to_iarray(int *arr, int *map, int length, int *cache);
+__device__ void   print_farr(float *arr, int length);
+__device__ void   print_iarr(int *arr, int length);
+__device__ void   invert_map(int *map, int length, int *cache);
+
+//////////
+// sort //
+//////////
+__device__ void   fswap(float *a, float *b);
+__device__ void   iswap(int *a, int *b);
+
+__device__ int    _quick_sort_iarr_by_fkey_partition(int *arr, float *key, int left, int right);
+__device__ int    _quick_sort_iarr_by_ikey_partition(int *arr, int *key, int left, int right);
+__device__ void   _quick_sort_iarr_by_fkey(int *arr, float *key, int left, int right);
+__device__ void   _quick_sort_iarr_by_ikey(int *arr, int *key, int left, int right);
+__device__ void   quick_sort_iarr_by_fkey(int *arr, float *key, int length);
+__device__ void   quick_sort_iarr_by_ikey(int *arr, int *key, int length);
+
+__device__ void   bubble_sort_iarr_by_fkey(int *arr, float *key, int length);
+__device__ void   bubble_sort_iarr_by_ikey(int *arr, int *key, int length);
+
+__device__ int*   arg_fsort(float *arr, int length, int *map, float *cache);
+__device__ int*   arg_isort(int *arr, int length, int *map, int *cache);
+
+///////////
+// utils //
+///////////
+__device__ float* copy_farray(float *arr, int length, float *cache)
 {
-    float *cpy;
-    cpy = new float [length];
+    float *cpy = cache;
     
     for(int i = 0; i < length; i++)
         cpy[i] = arr[i];
@@ -11,10 +45,9 @@ __device__ float* copy_farray(float *arr, int length)
     return cpy;
 }
 
-__device__ int* copy_iarray(int *arr, int length)
+__device__ int* copy_iarray(int *arr, int length, int *cache)
 {
-    int *cpy;
-    cpy = new int [length];
+    int *cpy = cache;
     
     for(int i = 0; i < length; i++)
         cpy[i] = arr[i];
@@ -32,6 +65,40 @@ __device__ void set_iarray_values(int *arr, int *values, int length)
     for(int i = 0; i < length; i++) arr[i] = values[i];
 }
 
+__device__ void apply_map_to_farray(float *arr, int *map, int length, float *cache)
+{
+    float *arr0 = copy_farray(arr, length, cache);
+    for(int i = 0; i < length; i++) arr[i] = arr0[map[i]];
+}
+
+__device__ void apply_map_to_iarray(int *arr, int *map, int length, int *cache)
+{
+    int *arr0 = copy_iarray(arr, length, cache);
+    for(int i = 0; i < length; i++) arr[i] = arr0[map[i]];
+}
+
+__device__ void print_farr(float *arr, int length)
+{
+    for(int i = 0; i < length; i++) printf("%f, ", arr[i]);
+    printf("\n");
+}
+
+__device__ void print_iarr(int *arr, int length)
+{
+    for(int i = 0; i < length; i++) printf("%d, ", arr[i]);
+    printf("\n");
+}
+
+__device__ void invert_map(int *map, int length, int *cache)
+{
+    // invert the permutation map
+    int *map0 = copy_iarray(map, length, cache);
+    for(int i = 0; i < length; i++) map[map0[i]] = i;
+}
+
+//////////
+// sort //
+//////////
 __device__ void fswap(float *a, float *b)
 {
     float t = *a;
@@ -64,42 +131,6 @@ __device__ int _quick_sort_iarr_by_fkey_partition(int *arr, float *key, int left
     return i+1;
 }
 
-__device__ void _quick_sort_iarr_by_fkey(int *arr, float *key, int left, int right)
-{
-    if(left < right)
-    {
-        int p = _quick_sort_iarr_by_fkey_partition(arr, key, left, right);
-        _quick_sort_iarr_by_fkey(arr, key, left, p-1);
-        _quick_sort_iarr_by_fkey(arr, key, p+1, right);
-    }
-}
-
-__device__ void quick_sort_iarr_by_fkey(int *arr, float *key, int length)
-{
-    _quick_sort_iarr_by_fkey(arr, key, 0, length-1);
-}
-
-__device__ int* arg_fsort(float *arr, int length)
-{
-    int *map = new int [length];
-    float *key = copy_farray(arr, length);
-    
-    for(int i = 0; i < length; i++) map[i] = i;
-    
-    quick_sort_iarr_by_fkey(map, key, length);
-    
-    delete key;
-    return map;
-}
-
-
-
-
-
-
-
-
-
 __device__ int _quick_sort_iarr_by_ikey_partition(int *arr, int *key, int left, int right)
 {
     float pivot = key[right];
@@ -118,6 +149,16 @@ __device__ int _quick_sort_iarr_by_ikey_partition(int *arr, int *key, int left, 
     return i+1;
 }
 
+__device__ void _quick_sort_iarr_by_fkey(int *arr, float *key, int left, int right)
+{
+    if(left < right)
+    {
+        int p = _quick_sort_iarr_by_fkey_partition(arr, key, left, right);
+        _quick_sort_iarr_by_fkey(arr, key, left, p-1);
+        _quick_sort_iarr_by_fkey(arr, key, p+1, right);
+    }
+}
+
 __device__ void _quick_sort_iarr_by_ikey(int *arr, int *key, int left, int right)
 {
     if(left < right)
@@ -128,49 +169,55 @@ __device__ void _quick_sort_iarr_by_ikey(int *arr, int *key, int left, int right
     }
 }
 
+__device__ void quick_sort_iarr_by_fkey(int *arr, float *key, int length)
+{
+    _quick_sort_iarr_by_fkey(arr, key, 0, length-1);
+}
+
 __device__ void quick_sort_iarr_by_ikey(int *arr, int *key, int length)
 {
     _quick_sort_iarr_by_ikey(arr, key, 0, length-1);
 }
 
-__device__ int* arg_isort(int *arr, int length)
+__device__ void bubble_sort_iarr_by_fkey(int *arr, float *key, int length)
 {
-    int *map = new int [length];
-    int *key = copy_iarray(arr, length);
-    
+    for(int i = 0; i < length-1; i++)
+        for(int j = 0; j < length-1-i; j++)
+            if(arr[j] > arr[j+1])
+            {
+                iswap(&arr[j], &arr[j+1]);
+                fswap(&key[j], &key[j+1]);
+            }
+}
+__device__ void bubble_sort_iarr_by_ikey(int *arr, int *key, int length)
+{
+    for(int i = 0; i < length-1; i++)
+        for(int j = 0; j < length-1-i; j++)
+            if(arr[j] > arr[j+1])
+            {
+                iswap(&arr[j], &arr[j+1]);
+                iswap(&key[j], &key[j+1]);
+            }
+}
+
+__device__ int* arg_fsort(float *arr, int length, int *map, float *cache)
+{
+    float *key = copy_farray(arr, length, cache);
     for(int i = 0; i < length; i++) map[i] = i;
-    
-    quick_sort_iarr_by_ikey(map, key, length);
-    
-    delete key;
+    bubble_sort_iarr_by_fkey(map, key, length); // on gpu we use bubble sort not quick sort
+    return map;
+}
+__device__ int* arg_isort(int *arr, int length, int *map, int *cache)
+{
+    int *key = copy_iarray(arr, length, cache);
+    for(int i = 0; i < length; i++) map[i] = i;
+    bubble_sort_iarr_by_ikey(map, key, length); // on gpu we use bubble sort not quick sort
     return map;
 }
 
-__device__ void invert_map(int *map, int length)
-{
-    int *map0 = copy_iarray(map, length);
-    for(int i = 0; i < length; i++) map[map0[i]] = i;
-    delete map0;
-}
-
-__device__ void apply_map_to_farray(float *arr, int *map, int length)
-{
-    float *arr0 = copy_farray(arr, length);
-    for(int i = 0; i < length; i++) arr[i] = arr0[map[i]];
-    delete arr0;
-}
-
-__device__ void apply_map_to_iarray(int *arr, int *map, int length)
-{
-    int *arr0 = copy_iarray(arr, length);
-    for(int i = 0; i < length; i++) arr[i] = arr0[map[i]];
-    delete arr0;
-}
-
-
-
-
-
+//////////
+// main //
+//////////
 __global__ void calculate_recomb(
     float *X,
     float *Y,
@@ -196,6 +243,7 @@ __global__ void calculate_recomb(
     int length = right - left;
     int num_grid = 2 * length - 1;
     
+    // make offsets
     X = &X[left];
     Y = &Y[left];
     Ed = &Ed[left];
@@ -212,8 +260,8 @@ __global__ void calculate_recomb(
     weight = &weight[offset];
     fcache = &fcache[left*2];
     icache = &icache[left*2];
-    
-    
+
+    // model parameters
     float alpha = params[0];
     float beta = params[1];
     
@@ -227,28 +275,24 @@ __global__ void calculate_recomb(
         Y_edge_ID[i*2] = Y_edge_ID[i*2+1] = i;
     }
     
-    int *map = arg_fsort(X_edge, length*2);
-    apply_map_to_farray(X_edge, map, length*2);
-    apply_map_to_iarray(X_edge_ID, map, length*2);
-    delete map;
-    int *tmp = arg_isort(X_edge_ID, length*2);
+    int *map = arg_fsort(X_edge, length*2, icache, fcache);
+    apply_map_to_farray(X_edge, map, length*2, fcache);
+    apply_map_to_iarray(X_edge_ID, map, length*2, &icache[2*length]);
+    int *tmp = arg_isort(X_edge_ID, length*2, icache, &icache[2*length]);
     set_iarray_values(X_edge_ID, tmp, length*2);
-    delete tmp;
     for(int i = 0; i < length; i++)
         if(X_edge_ID[i*2] > X_edge_ID[i*2+1])
             iswap(&X_edge_ID[i*2], &X_edge_ID[i*2+1]);
             
-    map = arg_fsort(Y_edge, length*2);
-    apply_map_to_farray(Y_edge, map, length*2);
-    apply_map_to_iarray(Y_edge_ID, map, length*2);
-    delete map;
-    tmp = arg_isort(Y_edge_ID, length*2);
+    map = arg_fsort(Y_edge, length*2, icache, fcache);
+    apply_map_to_farray(Y_edge, map, length*2, fcache);
+    apply_map_to_iarray(Y_edge_ID, map, length*2, &icache[2*length]);
+    tmp = arg_isort(Y_edge_ID, length*2, icache, &icache[2*length]);
     set_iarray_values(Y_edge_ID, tmp, length*2);
-    delete tmp;
     for(int i = 0; i < length; i++)
         if(Y_edge_ID[i*2] > Y_edge_ID[i*2+1])
             iswap(&Y_edge_ID[i*2], &Y_edge_ID[i*2+1]);
-            
+
     for(int i = 0; i < length; i++)
     {
         int x_left = X_edge_ID[i*2];
@@ -262,64 +306,13 @@ __global__ void calculate_recomb(
                 weight[indx + indy * num_grid] += density;
     }
     
-    
     // test
     for(int indx = 0; indx < num_grid; indx++)
         for(int indy = 0; indy < num_grid; indy++)
             weight[indx + indy * num_grid] *= (X_edge[indx+1]-X_edge[indx])*(Y_edge[indy+1]-Y_edge[indy]);
-    
+
     float r = 0;
     for(int i = 0; i < num_grid*num_grid; i++)
         r += weight[i];
     recomb[event_id]=r;
-    
-    
-    
-    
-    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
